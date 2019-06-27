dd559273989f2a08e8371c724e82bb6779e30c63
Ann Bailey <ann.bailey@duke.edu>
2019-06-27 11:18:18 -0400

    edited controller, index, edit for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,44 @@
 class CommentsController < ApplicationController
 
     def index
-        print "Hello world"
+        @comments = Comment.all
     end
 
+    def edit
+        @comment = Comment.find(params[:id])
+    end
+
+    def update
+        @comment = Comment.find(params[:id])
+        @comment.update(comment_params)
+        redirect_to comments_path
+    end
+
+    def show
+        @comment = Comment.find(params[:id])
+    end
+
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.save
+        redirect_to comments_path
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        redirect_to comments_path
+    end
+
+
+
+    #Define the parameters of a comment
+    private def comment_params
+        params.require(:comment).permit(:title, :body, :user_id, :issue_id)
+    end
+    
 end
