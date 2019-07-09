977fadb635c02a51b1e5d48d5e37feccea2ccd36
Alejandro Meza <aam79@duke.edu>
2019-07-09 13:48:51 -0400

    added likes and favorites

--- app/graphql/mutations/issues/add_like_to_issue.rb
+++ app/graphql/mutations/issues/add_like_to_issue.rb
@@ -2,7 +2,7 @@ module Mutations
     module Issues
         class AddLikeToIssue < BaseMutation
             
-            argument :id, ID, required: true
+            argument :id, Int, required: true
 
             type Types::IssueType
 
